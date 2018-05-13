82ebe6aebe653a636ff573df58091f86adce3710
Puru Dahal <puru@dahal.me>
2018-05-13 14:17:56 -0700

    initial setup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,62 @@
 <template>
   <div>
+    <div class="container">
+      <header class="navbar">
+        <section class="navbar-center">
+          <h3 class="logo">Domazone</h3>
+        </section>
+      </header>
+    </div>
+
+    <div class="hero bg-primary text-light text-center">
+      <div class="container">
+        <h1>Available dictionary .app domains</h1>
+
+        <!-- <div class="has-icon-right search centered">
+          <input type="text" class="form-input input-lg search-input" placeholder="Search for domain">
+          <i class="form-icon loading"></i>
+        </div> -->
+      </div>
+    </div>
+
     <nuxt/>
   </div>
 </template>
 
-<style lang="sass">
-  @import 'spectre.css'
-  .container
-    max-width: 900px
-    padding: 10px 5%
+<style lang="scss">
+$primary-color: red;
+
+@import "spectre.css";
+.navbar {
+  padding: 10px 0;
+}
+
+h2.logo {
+  margin: 0px !important;
+}
+
+.container{
+  max-width: 1080px;
+  padding: 0 5%
+}
+
+.hero {
+  padding: 10% 0;
+
+  .search {
+    max-width: 500px;
+  }
+
+  .search-input {
+    border-radius: 50px;
+    padding: 25px;
+  }
+}
+
+hr {
+  border: 0;
+  height: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,116 @@
 <template>
-  <section class="container">
-    <div>
-      
+  <div class="domains">
+    <div class="container">
+      <div class="domain-list">
+        <div class="columns">
+          <div class="column col-xs-6 col-md-12">
+            <div class="popover popover-bottom">
+              <button class="btn">Filters</button>
+              <div class="popover-container">
+                <div class="card">
+                  <div class="card-header">
+                    Length
+                    <div class="form-group">
+                      <label class="form-checkbox" v-for="length in [2, 3, 4, 5, 6]" :key="length">
+                        <input type="checkbox" :value = "length" v-model = "filters.length" >
+                        <i class="form-icon" ></i> {{ length }} letters
+                      </label>
+                    </div>
+                  </div>
+                  <hr>
+
+                  <div class="card-body">
+                    Starts with
+                    <div class="form-group">
+                      <label class="form-checkbox" v-for = "letter in 'abcdefghijklmnopqrstuvwxyz'.split('')" :key="letter">
+                        <input type="checkbox" :value = "letter" v-model = "filters.starts">
+                        <i class="form-icon"></i> {{ letter.toUpperCase() }}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <span class="chip" v-for="(letter, index) in filters.starts" :key="letter">
+              Starts with {{letter.toUpperCase()}}
+              <a @click ="removeLetter(index)" class="btn btn-clear" aria-label="Close" role="button"></a>
+            </span>
+
+            <span class="chip" v-for="(l, index) in filters.length" :key="l">
+              {{l}} letters
+              <a @click="removeLength(index)" class="btn btn-clear" aria-label="Close" role="button"></a>
+            </span>
+          </div>
+        </div>
+        <br>
+
+        <div class="columns">
+          <div class="column col-4 col-md-6 col-xs-12" v-for="(app, index) in domains" :key="index">
+            <div class="card">
+              <div class="card-header">
+                <button class="btn float-right btn-sm">Buy Now</button>
+                <div class="card-title h5">{{ app.domain }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
 
+import http from 'axios'
+const querystring = require('querystring')
+
 export default {
+  data: function() {
+    return {
+      filters: {
+        length: [],
+        starts: [],
+        last_evel_key: null
+      },
+      domains: Object,
+    }
+  },
+  created() {
+    http.get('http://localhost:3000/domains?' + querystring.stringify(this.filters))
+        .then(function (response) {
+          this.last_evel_key = response.data.last_eval_key
+          console.log(this.last_evel_key)
+          this.domains = response.data.domains
+        }.bind(this))
+        .catch(function (error) {
+          console.log(error);
+        });
+  },
+
+  methods: {
+    removeLength(index) {
+      this.filters.length.splice(index, 1)
+    },
+
+    removeLetter(index) {
+      debugger
+      this.filters.starts.splice(index, 1)
+    }
+  },
+
   components: {
   }
 }
 </script>
 
-<style>
+<style lang="sass">
+  .domains
+    margin: 3% 0
 
+  .column
+    margin-bottom: 20px
+
+  .domain-list
+    margin: 5% 0
 </style>
